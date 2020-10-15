7392ffe8c827456ce8a58f6de623d5124032c539
pedro.rodrigues <pedroh.mix@gmail.com>
2020-10-15 18:16:02 -0300

    Adicionando o sugar DB

--- app/src/main/java/com/projeto_padrao/models/Usuario.java
+++ app/src/main/java/com/projeto_padrao/models/Usuario.java
@@ -3,14 +3,27 @@ package com.projeto_padrao.models;
 import android.content.Context;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.core.content.ContextCompat;
+
+import com.orm.SugarRecord;
+import com.projeto_padrao.R;
 import com.projeto_padrao.RegisterActivity;
 
-public class Usuario {
+import java.util.List;
+
+public class Usuario extends SugarRecord {
     private String username;
     private String password;
     private boolean logado;
+
+    //É OBRIGATÓRIO A CRIAÇÃO DE UM CONSTRUTOR VAZIO
+    public Usuario(){
+
+    }
 
     public Usuario(String usuario, String senha) {
         this.username = usuario;
@@ -45,13 +58,15 @@ public class Usuario {
 
         Android android = new Android(context);
         if (android.getConected(context)){
-
-            this.logado = true;
-            Log.d("credenciais", " \nLogin: " + this.username+"\nSenha: " + this.password);
-            Toast.makeText(context,"Usuario está logado",Toast.LENGTH_LONG).show();
+            List<Usuario> usuarios = Usuario.find(Usuario.class, "username = ? and password = ?", this.username, this.password);
+            if(usuarios.size()>0){
+                this.logado = true;
+            }else {
+                this.logado = false;
+                Toast.makeText(context,"Usuario e senha incorretos",Toast.LENGTH_LONG).show();
+            }
 
         }else {
-
             this.logado = false;
             Log.d("credenciais", " \nLogin: " + this.username+"\nSenha: " + this.password);
             Toast.makeText(context,"Usuario não está logado",Toast.LENGTH_LONG).show();
@@ -60,5 +75,11 @@ public class Usuario {
     }
 
     public void registrar(Context context) {
+        List<Usuario> usuarios = Usuario.find(Usuario.class, "username = ? and password = ?", this.username, this.password);
+        if(usuarios.size()==0){
+            this.save();
+        }else {
+            Toast.makeText(context,"Este usuário já existe",Toast.LENGTH_SHORT).show();
+        }
     }
 }
