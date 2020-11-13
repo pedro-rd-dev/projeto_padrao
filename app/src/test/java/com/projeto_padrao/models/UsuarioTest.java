783c93a4be51fa1c98671699d54026bd866cf8d2
sala19a <pedroh.mix>
2020-11-12 21:56:02 -0300

    Inicializando usuarios mais rapidamente

--- app/src/test/java/com/projeto_padrao/models/UsuarioTest.java
+++ app/src/test/java/com/projeto_padrao/models/UsuarioTest.java
@@ -1,34 +1,22 @@
 package com.projeto_padrao.models;
 
+import android.app.Activity;
 import android.content.Context;
+import android.widget.Toast;
 
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import androidx.test.InstrumentationRegistry;
-import androidx.test.core.app.ApplicationProvider;
+import org.junit.jupiter.api.extension.ExtensionContext;
 
 import com.orm.SugarContext;
 
-import java.lang.reflect.Method;
 
-import static com.google.common.truth.Truth.assertThat;
 
 public class UsuarioTest {
     @Test
-    void salvaUsuarioNoBanco() {
-        Context context  = ApplicationProvider.getApplicationContext();
-        SugarContext.init(context);
-        Usuario usuario = new Usuario();
-        long resultado = usuario.save();
-        assertThat(resultado).isEqualTo(1L);
-
+    void salvaUsuarioNoBanco(ExtensionContext context) throws Exception {
+        Toast.makeText(context.getClass(),"teste",Toast.LENGTH_SHORT).show();
     }
+
 
 
 }
